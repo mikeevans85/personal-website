406890ad916771867f5f2ced86c835919c4f6a11
mikeevans85 <baudecordier@gmail.com>
2019-04-13 16:41:09 -0500

    Added video

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from "axios";
+import vPlayBack from 'v-playback';
+Vue.use(vPlayBack);
 
 axios.defaults.baseURL = 
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
